91bd196a2cb1f15fd3d3b3575d0ca27befb8a89a
unknown <jason.wong6507@gmail.com>
2022-11-13 11:06:19 -0500

    Got current location to start working in the javascript

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //console.log(clientId)
-//getCoordinates()
+getCoordinates()
 //Need to get the user's current location
 async function getCoordinates(){
     latlong=await new Promise ((resolve, reject)=>{
@@ -8,15 +8,20 @@ async function getCoordinates(){
     //need to get teh latitude and longitude of the current position
     return [latlong.coords.latitude, latlong.coords.longitude]
 }
-//console.log(getCoordinates())
+console.log(getCoordinates())
 
-const myMap = L.map('map', {
-    center: [48.868672, 2.342130],
-    zoom: 12,
-});
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: '15',
-}).addTo(myMap)
+window.onload = async()=>{
+    let cooridnates = await getCoordinates()
+    const myMap = L.map('map', {
+        center: cooridnates,
+        zoom: 12,
+    });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: '15',
+    }).addTo(myMap)
+
+    var marker = L.marker(cooridnates).addTo(myMap);
+}
 
 //var map= L.map('map').setView(getCoordinates(), 13);
