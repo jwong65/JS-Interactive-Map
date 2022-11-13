bae2f7ba0c85103cdf9af44174fff5887ebb1db8
unknown <jason.wong6507@gmail.com>
2022-11-13 10:40:46 -0500

    Added the geolocation async function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(clientId)
+//console.log(clientId)
+getCoordinates()
+//Need to get the user's current location
+async function getCoordinates(){
+    latlong=await new Promise ((resolve, reject)=>{
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+    //need to get teh latitude and longitude of the current position
+    return [latlong.coords.latitude, latlong.coords.longitude]
+}
+console.log(getCoordinates())
+
+var map= L.map('map').setView(getCoordinates(), 13);
